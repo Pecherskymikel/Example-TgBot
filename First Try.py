1843d6c100fd3f2f8a8f51d73a7f4ef0ca24cfee
Pechersky Mikel <75828885+Pecherskymikel@users.noreply.github.com>
2021-04-14 20:52:05 +0300

    Add files via upload

---
+++ First Try.py
@@ -0,0 +1,53 @@
+#We have elready fucked this code for [3] hour
+#When we started writing this code, only we and God understood what was written here. To be continued (at the end of the code)
+
+import telebot
+from telebot import types
+
+#1768045491:AAHwrAOzQU9oOQvGpK0Lb4egMmIrt4ihT7k
+
+name = ''
+surname = ''
+age = 0
+
+bot = telebot.TeleBot("1768045491:AAHwrAOzQU9oOQvGpK0Lb4egMmIrt4ihT7k")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):  #здесь мы хотим вывести Привет
+	#bot.reply_to(message, "Привет, чтобы узнать, что я умею вбей /skills")
+    bot.send_message(message.chat.id,"Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом.\nЧтобы узнать мой функционал вбей <b>/skills</b>".format(message.from_user, bot.get_me()), parse_mode='html')
+
+@bot.message_handler(commands=['skills', 'help'])
+def welcome(message):
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Купить")
+    item2 = types.KeyboardButton("О нас")
+    item3 = types.KeyboardButton("Помощь")
+    item4 = types.KeyboardButton("Каталог")
+
+    markup.add(item1, item2, item3, item4)
+
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот созданный чтобы быть подопытным кроликом.".format(message.from_user, bot.get_me()), parse_mode='html', reply_markup=markup)
+
+
+@bot.message_handler(content_types=['text'])
+def lalala(message):
+        if message.text == "Купить":
+            bot.send_message(message.chat.id, "Какой товар хотите купить?")
+        elif message.text == 'О нас':
+
+            markup = types.InlineKeyboardMarkup(row_width=2)
+            item1 = types.InlineKeyboardButton("Хорошо", callback_data='good')
+            item2 = types.InlineKeyboardButton("Не очень", callback_data='bad')
+
+            markup.add(item1, item2)
+
+            bot.send_message(message.chat.id, 'Отлично, сам как?', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Я не знаю что ответить 😢')
+
+
+bot.polling()
+
+#Now only God remains
